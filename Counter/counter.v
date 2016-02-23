f11034275c30da6b1f09b226c39306ef7111e223
Alex Bucknall <alex.bucknall@gmail.com>
2016-02-23 23:58:08 +0000

    Added Counter

---
+++ Counter/counter.v
@@ -0,0 +1,14 @@
+module counter (input clk, rst, en, dn, output reg [3:0] count);
+
+always@(posedge clk)
+begin
+if(rst)
+count <= 4'b0000;
+else
+  if(en)
+    if(dn)
+	count <= count - 1'b1;
+  else
+	count <= count + 1'b1;
+end
+endmodule
